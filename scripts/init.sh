5b77b269ebe2a8b7cbe62a50cc4b6dc23a3649cd
정석호 <creaticoding@gmail.com>
2023-08-15 00:35:23 +0900

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,7 +3,7 @@ cd ~
 curl https://creaticoding.github.io/creco-manual/scripts/gitconfig -o .gitconfig
 
 git config --global user.name 정석호
-git config --global user.email seokho.jeong@toss.im
+git config --global user.email creaticoding@gmail.com
 git config --global commit.gpgsign true
 git config --global pull.ff only
 git config --global user.signingkey $(gpg --list-secret-keys --keyid-format LONG | grep sec | awk '{ print $2 }' | cut -d "/" -f 2)
