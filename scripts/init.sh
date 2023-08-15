a9ea2351bbeb89a85cfee8a60451a0c11dcc6c4f
정석호 <creaticoding@gmail.com>
2023-08-15 12:34:10 +0900

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -12,7 +12,7 @@ git config --global gpg.program /usr/local/MacGPG2/bin/gpg2
 cat ~/.gitconfig
 
 echo "" >> .ssh/config
-echo "Host divopsgit" >> .ssh/config
+echo "Host divopsor" >> .ssh/config
 echo "  Hostname github.com" >> .ssh/config
 echo "  IdentityFile ~/.ssh/id_rsa-divopsor" >> .ssh/config
 echo "  IdentitiesOnly yes" >> .ssh/config
