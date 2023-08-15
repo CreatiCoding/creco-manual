09748e8c8092369cbedfccc3c25ad0fec28a6945
정석호 <creaticoding@gmail.com>
2023-08-15 12:37:51 +0900

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,7 +7,8 @@ git config --global user.email creaticoding@gmail.com
 git config --global commit.gpgsign true
 git config --global pull.ff only
 git config --global user.signingkey $(gpg --list-secret-keys --keyid-format LONG | grep sec | awk '{ print $2 }' | cut -d "/" -f 2)
-git config --global gpg.program /usr/local/MacGPG2/bin/gpg2
+# git config --global gpg.program /usr/local/MacGPG2/bin/gpg2
+git config --global gpg.program "$(which gpg)"
 
 cat ~/.gitconfig
 
