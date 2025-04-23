e952e5ed0291a4fb574330f71cdbf8248209790b
정석호 <nodejsdeveloper@kakao.com>
2025-04-24 02:51:20 +0900

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,12 +3,9 @@ cd ~
 curl https://creaticoding.github.io/creco-manual/scripts/gitconfig -o .gitconfig
 
 git config --global user.name 정석호
-git config --global user.email creaticoding@gmail.com
+git config --global user.email nodejsdeveloper@kakao.com
 git config --global commit.gpgsign true
 git config --global pull.ff only
-git config --global user.signingkey $(gpg --list-secret-keys --keyid-format LONG | grep sec | awk '{ print $2 }' | cut -d "/" -f 2)
-# git config --global gpg.program /usr/local/MacGPG2/bin/gpg2
-git config --global gpg.program "$(which gpg)"
 
 cat ~/.gitconfig
 
@@ -17,3 +14,10 @@ echo "Host divopsor" >> .ssh/config
 echo "  Hostname github.com" >> .ssh/config
 echo "  IdentityFile ~/.ssh/id_rsa-divopsor" >> .ssh/config
 echo "  IdentitiesOnly yes" >> .ssh/config
+
+echo "" >> .ssh/config
+echo "Host creaticoding" >> .ssh/config
+echo "  Hostname github.com" >> .ssh/config
+echo "  IdentityFile ~/.ssh/id_rsa-creaticoding" >> .ssh/config
+echo "  IdentitiesOnly yes" >> .ssh/config
+
